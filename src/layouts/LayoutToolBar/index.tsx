a45110c44054a49be30f5b333d981d0edcd51c45
wangfanghua <1418202350@qq.com>
2020-09-23 19:12:48 +0800

    添加：添加字节跳动图标库@icon

--- src/layouts/LayoutToolBar/index.tsx
+++ src/layouts/LayoutToolBar/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import classes from  './index.less';
-import { SmileTwoTone } from '@ant-design/icons'
+import React from 'react';
+
+import Style from './index.less';
+
+import { SmileTwoTone } from '@ant-design/icons';
+import { Hexagonal } from '@icon-park/react';
 
 export default function LayoutToolBars() {
-	return (
-		<div className={ classes.layoutHeader }>
-			<div className={ classes.logo }>
-				<SmileTwoTone />
-			</div>
-		</div>
-	)
+  return (
+    <div className={Style.layoutHeader}>
+      <div className={Style.logo}>
+        <SmileTwoTone />
+        <Hexagonal
+          theme="multi-color"
+          size="24"
+          fill={['#333', '#2F88FF', '#FFF', '#43CCF8']}
+        />
+      </div>
+    </div>
+  );
 }
